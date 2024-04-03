52500c3b02a7f38c33b9b8f5b3642d97a769f8bf
Victor Kemboi <vickemtoo97@gmail.com>
2024-04-03 20:56:02 +0300

    updating DOM

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener ("DOMContentLoaded", () =>{
+    DOMupdate ()
+})
+
+ DOMupdate =() => {
+    document.getElementById('text').innerHTML="This is really cool!";
+}
